d0426242c99f0c1a7cbfa9169e58f71bb0c1da41
enricllagostera <enricllagostera@gmail.com>
2014-09-29 15:43:12 -0300

    novo gitignore, olamundo

---
+++ Exemplo_OlaMundo/Assets/OlaMundo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OlaMundo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// print ("Ola mundo!!!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print ("Ola Mundo no update");
+	}
+}
---
+++ Exemplo_OlaMundo/Assets/Variaveis.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Variaveis : MonoBehaviour {
+
+	public bool escreverMsg;
+	public int contador;
+	public float tempo;
+	public string nome;
+
+	// Use this for initialization
+	void Start () {
+		escreverMsg = true;
+		contador = 0;
+		tempo = 0;
+		// nome = "bob";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		tempo += Time.deltaTime;
+
+		if (escreverMsg) {
+			print ("a personagem se chama " + nome);
+			contador = contador + 1;
+		}
+	}
+}
