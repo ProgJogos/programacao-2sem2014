61ac1048c2b4cd3934d1f60632008dc932dab5b0
enricllagostera <enricllagostera@gmail.com>
2014-10-01 07:31:01 -0300

    variaveis da unity, game object

---
+++ Exemplo_Variaveis/Assets/GeradorGameObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeradorGameObject : MonoBehaviour {
+
+	public GameObject novoGO;
+
+	// Use this for initialization
+	void Start () {
+		novoGO = new GameObject ("novoGO");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Exemplo_Variaveis/Assets/VariaveisDaUnity.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariaveisDaUnity : MonoBehaviour {
+
+	public Vector3 deslocamento;
+	public Vector3 giro;
+	public Color cor1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// transform.position += deslocamento;
+		// Vector3 rotacaoAntiga = transform.rotation.eulerAngles;
+		// transform.rotation = Quaternion.Euler (giro);
+		transform.Translate (deslocamento);
+		transform.Rotate (giro);
+		// cor1.r += 0.01f;
+		// Color corCamera = Camera.main.backgroundColor;
+		// Color corTemporaria = Color.Lerp (corCamera, cor1, 0.05f);
+		// Camera.main.backgroundColor = corTemporaria;
+		cor1.g = Mathf.Sin (Time.time);
+		Camera.main.backgroundColor = cor1;
+	}
+}
