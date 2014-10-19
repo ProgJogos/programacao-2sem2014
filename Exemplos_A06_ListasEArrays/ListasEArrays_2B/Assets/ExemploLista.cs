261f36b92eec6331a5a955fdeaf332ba8bcafad0
Enric Llagostera <enricllagostera@gmail.com>
2014-10-19 20:58:47 -0200

    renames e exemplo listas

---
+++ Exemplos_A06_ListasEArrays/ListasEArrays_2B/Assets/ExemploLista.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ExemploLista : MonoBehaviour 
+{
+	public List<int> numeros;
+
+	void Start () 
+	{
+		numeros = new List<int>();
+		numeros.Add (100); 
+		numeros.Add (200);
+		numeros.Remove(100);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
