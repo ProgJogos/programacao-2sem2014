a7a5ffa01f3e72dbd17c4a9f586d7d9ad8f5d5f0
Enric Llagostera <enricllagostera@gmail.com>
2014-10-23 18:51:28 -0200

    listas e arrays - exemplos com arrays

---
+++ Exemplos_A06_ListasEArrays/ListasEArrays_2B/Assets/ListaGO.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ListaGO : MonoBehaviour 
+{
+	// molde de GO
+	public GameObject bloco; 
+	public List<GameObject> fileira;
+	
+	void Start () 
+	{
+		Vector3 pos = Vector3.zero;
+		fileira = new List<GameObject>();
+		while (fileira.Count < 10)
+		{
+			GameObject novo = Instantiate (
+				bloco, 
+				pos, 
+				Quaternion.identity) as GameObject;
+			fileira.Add(novo);
+			pos.x += 2f;
+		}
+	}
+	
+	void Update () 
+	{
+		foreach (GameObject elemento in fileira)
+		{
+			Vector3 novaPos = elemento.transform.position;
+			novaPos.y = Mathf.Sin(Time.time + novaPos.x*0.3f) * 4f;
+			elemento.transform.position = novaPos;
+		}
+	}
+}
+
+
+
+
---
+++ Exemplos_A06_ListasEArrays/ListasEArrays_2B/Assets/Tabuleiro.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tabuleiro : MonoBehaviour 
+{
+	// declaracao do array
+	public int[] casas;
+	
+	void Start () 
+	{
+		// inicializei o array
+		casas = new int[10];
+		
+		int pontos = 10;
+		
+		for (int indice = 0; indice < 10; indice++)
+		{
+			if (indice % 2 == 0)
+			{
+				casas[indice] = pontos;
+				pontos += 10;
+			}
+		}
+	}
+}
