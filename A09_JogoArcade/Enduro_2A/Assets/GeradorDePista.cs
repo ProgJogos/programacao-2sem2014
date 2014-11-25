4d76488d758644ab6087d7fd1b91f1f436298673
Enric Llagostera <enricllagostera@gmail.com>
2014-11-25 17:01:50 -0200

    exemplo enduro

---
+++ A09_JogoArcade/Enduro_2A/Assets/GeradorDePista.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GeradorDePista : MonoBehaviour {
+
+	public Trecho[] prefabsTrechos;
+	public int nroTrechos;
+	public List<Trecho> pista;
+
+	void Start () {
+
+		pista = new List<Trecho> ();
+
+		// criar 1o trecho
+		GameObject novoTrecho = Instantiate (
+			prefabsTrechos [1].gameObject,
+			Vector3.zero,
+			Quaternion.identity) as GameObject;
+
+		pista.Add( novoTrecho.GetComponent<Trecho>() );
+
+		while (pista.Count < nroTrechos) 
+		{
+			GameObject segmento = Instantiate (
+				prefabsTrechos [Random.Range(0, 2)].gameObject,
+				pista[pista.Count - 1].encaixe.position,
+				pista[pista.Count - 1].encaixe.rotation) 
+				as GameObject;
+			pista.Add( segmento.GetComponent<Trecho>() );
+		}
+	}
+}
---
+++ A09_JogoArcade/Enduro_2A/Assets/Movimento.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimento : MonoBehaviour {
+
+	public float velocidade;
+	public LayerMask mascara;
+
+	void Update () {
+
+		RaycastHit hit;
+
+		if (Physics.Raycast (
+				transform.position, 
+				-Vector3.up, 
+				out hit, 
+				3f, 
+				mascara)) 
+		{
+			//transform.rotation = hit.transform.rotation;
+			transform.rotation = Quaternion.Slerp (
+					transform.rotation, hit.transform.rotation, 
+					Time.deltaTime * 10);
+		}
+
+		//
+
+		transform.Translate (
+			Input.GetAxis ("Horizontal"), 
+			0, 
+			velocidade, 
+			Space.Self);
+	}
+}
