fe535cc322ff339178dcacc8692a8acf0cef2125
Enric Llagostera <enricllagostera@gmail.com>
2015-02-13 10:26:36 -0200

    exemplos de graficos 2D

---
+++ A12_Graficos2D/Graficos2D_2A/Assets/Jogador.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jogador : MonoBehaviour {
+
+	public float velocidadeX;
+	public bool atirando = false;
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Space) &&
+		    rigidbody2D.velocity.magnitude == 0) {
+			atirando = true;
+			GetComponent<Animator> ().SetTrigger("Atirou");
+		}
+	}
+
+	void FixedUpdate () {
+		// calcula velocidade baseado no input
+		float vel = velocidadeX * Input.GetAxis ("Horizontal");
+		// aplica velocidade no rigidbody2D
+		rigidbody2D.velocity = new Vector2 (
+			vel, rigidbody2D.velocity.y);
+		// muda o parametro do animator para o 
+		// modulo da velocidade
+		GetComponent<Animator> ().SetFloat ("VelocidadeX", 
+        	Mathf.Abs(vel)); 
+		// soh muda de direcao se estiver se movendo
+		if (Mathf.Abs (vel) > 0) {
+			transform.localScale = new Vector3 (
+				Mathf.Sign (vel), 1, 1);
+		}
+		
+	}
+}
---
+++ A12_Graficos2D/Graficos2D_2B/Assets/Jogador.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jogador : MonoBehaviour {
+
+	public float velocidadeX;
+
+	void FixedUpdate () {
+		rigidbody2D.velocity = new Vector2 (
+			Input.GetAxis("Horizontal") * velocidadeX,
+			rigidbody2D.velocity.y);
+		var animator = GetComponent<Animator> ();
+		animator.SetFloat ("VelocidadeX", Mathf.Abs(rigidbody2D.velocity.x));
+		if (Mathf.Abs (rigidbody2D.velocity.x) > 0) {
+			transform.localScale = new Vector3 (
+				Mathf.Sign (rigidbody2D.velocity.x), 1, 1);
+		}
+	}
+}
