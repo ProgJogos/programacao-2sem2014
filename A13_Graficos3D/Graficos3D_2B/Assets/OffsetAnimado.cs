467f2c9fd3fff5c236f949cfba8f770369becbab
Enric Llagostera <enricllagostera@gmail.com>
2015-02-23 15:56:04 -0300

    gráficos 3d

---
+++ A13_Graficos3D/Graficos3D_2B/Assets/OffsetAnimado.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffsetAnimado : MonoBehaviour {
+
+	public Vector2 velocidade;
+		
+	void Update () {
+		renderer.material.mainTextureOffset +=
+			velocidade * Time.deltaTime;
+	}
+}
